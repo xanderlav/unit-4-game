4329fb94c6d02c8be793435ee8c4e81e4e59612f
@xanderlav <alejandroregino.hussmann.com>
2019-10-01 00:16:20 -0500

    Logic changes to improve score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,18 +8,29 @@ var valGem03 = 0;
 var valGem04 = 0;
 var ScoreIn = 0;
 var totVal = 0;
+var sigue = false;
+var txtResultado = "You loose ";
 
 var usrMessage = "";
-function topLimit(){
+function topLimit () {
     limSup = Math.floor(Math.random() * 300);
 }
 
-function randValues(){
+function randValues () {
     ScoreIn = Math.floor(Math.random() * 500);
     $("#topScore").html("<h2> Top Score is: " + ScoreIn + "</h2>");
      console.log("El tope inicial fue: " + ScoreIn);
 }
 
+function verify() {
+    sigue = confirm(txtResultado + "Would you like continue playing?");
+    console.log(sigue);
+    if (sigue == true) {
+        randValues();
+        totVal = 0;
+        $("#yourScore").html("<h2> Your score is: " + totVal + "</h2>");
+    }
+}
 // function randValue01(){ 
     // valGem02 = Math.floor(Math.random() * 50);
     // valGem03 = Math.floor(Math.random() * 25);
@@ -27,19 +38,47 @@ function randValues(){
 
 // }
 
+// Determine random value for teal gem
 $("#Tealgem").on("click", function(){
-    valGem01 = Math.floor(Math.random() * 150);
+    valGem01 = Math.floor(Math.random() * 100);
     totVal = totVal + valGem01;
-    $("#yourScore").html("<h2> Your score is: " + totVal + "</h2>")
-    if(totVal > ScoreIn){
+    $("#yourScore").html("<h2> Your score is: " + totVal + "</h2>");
+
+
+    if (totVal > ScoreIn) {
         contLos++;
-        alert("You loose!");
         $("#looser").html("<h2> Defeats: " + contLos + "</h2>");
+        txtResultado="You loose ";
+        verify();
     }
-    if(totVal === ScoreIn){
+    // When win add counter 
+    if (totVal === ScoreIn) {
         contWin++;
-        alert("You won!");
+        txtResultado = "You won!";
         $("#winner").html("<h2> Victories: " + contWin + "</h2>");
+        verify();
+    }
+});
+
+// Determine random value for yellow gem
+$("#Yellgem").on("click", function(){
+    valGem02 = Math.floor(Math.random() * 50);
+    totVal = totVal + valGem02;
+    $("#yourScore").html("<h2> Your score is: " + totVal + "</h2>");
+
+    if (totVal > ScoreIn) {
+        contLos++;
+        $("#looser").html("<h2> Defeats: " + contLos + "</h2>");
+        verify();
+        txtResultado="You loose ";        
+        }
+    
+    // When win add counter 
+    if (totVal === ScoreIn) {
+        contWin++;
+        txtResultado="You won! ";
+        $("#winner").html("<h2> Victories: " + contWin + "</h2>");
+        verify();
     }
 });
 
