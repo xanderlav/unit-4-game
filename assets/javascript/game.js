b0bb24885604e3c61df9bfb64ecf628a8bf6c93d
@xanderlav <alejandroregino.hussmann.com>
2019-09-30 19:37:32 -0500

    CSS Reset update definition

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Defining global variables
+var contLos = 0;
+var contWin = 0;
+var valGem01 = 0;
+var valGem02 = 0;
+var valGem03 = 0;
+var valGem04 = 0;
+
+var usrMessage = "";
+
+function randValues(){
+    var valGem01 = Math.floor(Math.random) * 100; 
+    var valGem02 = Math.floor(Math.random) * 50;
+    var valGem03 = Math.floor(Math.random) * 25;
+    var valGem04 = Math.floor(Math.random) * 5;
+}
+
